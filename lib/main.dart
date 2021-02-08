a8f7e7eb88f681941121b8090dfa59801f5d5ab1
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-09 02:45:21 +0600

    api modeling

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MainScreen(),
+      home: Home(),
     );
   }
 }
--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,14 +1,89 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MainScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/model.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<UsersDetail> _list = [];
+  var loading = false;
+  Future<Null> _fetchData() async {
+    setState(() {
+      loading = true;
+    });
+    final response =
+        await http.get("https://jsonplaceholder.typicode.com/users");
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        for (Map i in data) {
+          _list.add(UsersDetail.fromJson(i));
+        }
+        loading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.ac_unit), onPressed: () {}),
-        title: Text("Firebase CRud"),
-        elevation: 0,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: loading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: _list.length,
+                itemBuilder: (context, i) {
+                  final x = _list[i];
+                  return Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(x.name),
+                        Text(x.email),
+                        Text(x.phone),
+                        Text(x.website),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "Address",
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(x.address.street),
+                        Text(x.address.suite),
+                        Text(x.address.city),
+                        Text(x.address.zipcode),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Text(
+                          "Company",
+                          style: TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                        Text(x.company.name),
+                      ],
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
