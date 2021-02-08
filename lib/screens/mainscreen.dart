7f8a45b20fbe7837739fb1f92ea358edc58e930e
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-09 02:14:47 +0600

    chrome desktop enbled

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.ac_unit), onPressed: () {}),
+        title: Text("Firebase CRud"),
+        elevation: 0,
+      ),
+    );
   }
 }
