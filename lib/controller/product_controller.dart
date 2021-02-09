2b06acab24a83f0ad6e60d3639e87ffb06d5982a
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-09 11:37:40 +0600

    working with getx api calling with model

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,7 @@
+import 'package:fireandapi30days/model/productmodel.dart';
+import 'package:get/get.dart';
+
+class ProductController extends GetxController {
+  // ignore: deprecated_member_use
+  var productList = List<Product>().obs;
+}
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,65 +1,96 @@
-class UsersDetail {
-  final int id;
-  final String name;
-  final String username;
-  final String email;
-  final Address address;
-  final String phone;
-  final String website;
-  final Company company;
+class News {
+  String status;
+  int totalResults;
+  List<Articles> articles;
 
-  UsersDetail(
-      {this.id,
-      this.name,
-      this.username,
-      this.email,
-      this.address,
-      this.phone,
-      this.website,
-      this.company});
+  News({this.status, this.totalResults, this.articles});
 
-  factory UsersDetail.fromJson(Map<String, dynamic> json) {
-    return new UsersDetail(
-        id: json['id'],
-        name: json['name'],
-        username: json['username'],
-        email: json['email'],
-        address: Address.fromJson(json['address']),
-        phone: json['phone'],
-        website: json['website'],
-        company: Company.fromJson(json['company']));
+  News.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    totalResults = json['totalResults'];
+    if (json['articles'] != null) {
+      // ignore: deprecated_member_use
+      articles = new List<Articles>();
+      json['articles'].forEach((v) {
+        articles.add(new Articles.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['totalResults'] = this.totalResults;
+    if (this.articles != null) {
+      data['articles'] = this.articles.map((v) => v.toJson()).toList();
+    }
+    return data;
   }
 }
 
-class Address {
-  final String street;
-  final String suite;
-  final String city;
-  final String zipcode;
+class Articles {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
 
-  Address({this.street, this.suite, this.city, this.zipcode});
+  Articles(
+      {this.source,
+      this.author,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
 
-  factory Address.fromJson(Map<String, dynamic> json) {
-    return new Address(
-      street: json['street'],
-      suite: json['suite'],
-      city: json['city'],
-      zipcode: json['zipcode'],
-    );
+  Articles.fromJson(Map<String, dynamic> json) {
+    source =
+        json['source'] != null ? new Source.fromJson(json['source']) : null;
+    author = json['author'];
+    title = json['title'];
+    description = json['description'];
+    url = json['url'];
+    urlToImage = json['urlToImage'];
+    publishedAt = json['publishedAt'];
+    content = json['content'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.source != null) {
+      data['source'] = this.source.toJson();
+    }
+    data['author'] = this.author;
+    data['title'] = this.title;
+    data['description'] = this.description;
+    data['url'] = this.url;
+    data['urlToImage'] = this.urlToImage;
+    data['publishedAt'] = this.publishedAt;
+    data['content'] = this.content;
+    return data;
   }
 }
 
-class Company {
-  final String name;
-  final String catchPhrase;
-  final String bs;
+class Source {
+  String id;
+  String name;
 
-  Company({this.name, this.catchPhrase, this.bs});
-  factory Company.fromJson(Map<String, dynamic> json) {
-    return new Company(
-      name: json['name'],
-      catchPhrase: json['catchPhrase'],
-      bs: json['bs'],
-    );
+  Source({this.id, this.name});
+
+  Source.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
   }
 }
