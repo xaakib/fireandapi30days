d04f986f193d9bc28cea34e67ad0c7ed86e3e7be
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-11 00:23:35 +0600

    have some bug in routing

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:fireandapi30days/services/authenticationServices.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,11 +7,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  AuthenticationService _authenticationService = AuthenticationService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("HomeScreen"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.login_outlined),
+              onPressed: () {
+                _authenticationService.signOut();
+              })
+        ],
       ),
     );
   }
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -3,8 +3,6 @@ import 'package:fireandapi30days/services/authenticationServices.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'homescreen.dart';
-
 class LoginScreen extends StatefulWidget {
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -20,90 +18,95 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Form(
-            key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 30),
-                      TextFormField(
-                        controller: _emailContoller,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Email cannot be empty';
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.white)),
-                        style: TextStyle(color: Colors.white),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.deepPurple,
+            child: Center(
+              child: Form(
+                key: _key,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
                       ),
-                      SizedBox(height: 30),
-                      TextFormField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Password cannot be empty';
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.white)),
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      FlatButton(
-                        child: Text('Not registerd? Sign up'),
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            CupertinoPageRoute(
-                              fullscreenDialog: true,
-                              builder: (context) => RegistrationScreen(),
-                            ),
-                          );
-                        },
-                        textColor: Colors.white,
-                      ),
-                      SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Column(
                         children: [
-                          FlatButton(
-                            child: Text('Login'),
-                            onPressed: () {
-                              if (_key.currentState.validate()) {
-                                signInUser();
-                              }
+                          SizedBox(height: 30),
+                          TextFormField(
+                            controller: _emailContoller,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Email cannot be empty';
+                              } else
+                                return null;
                             },
-                            color: Colors.white,
+                            decoration: InputDecoration(
+                                labelText: 'Email',
+                                labelStyle: TextStyle(color: Colors.white)),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(height: 30),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Password cannot be empty';
+                              } else
+                                return null;
+                            },
+                            decoration: InputDecoration(
+                                labelText: 'Password',
+                                labelStyle: TextStyle(color: Colors.white)),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          FlatButton(
+                            child: Text('Not registerd? Sign up'),
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                CupertinoPageRoute(
+                                  fullscreenDialog: true,
+                                  builder: (context) => RegistrationScreen(),
+                                ),
+                              );
+                            },
+                            textColor: Colors.white,
+                          ),
+                          SizedBox(height: 30),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              FlatButton(
+                                child: Text('Login'),
+                                onPressed: () {
+                                  if (_key.currentState.validate()) {
+                                    signInUser();
+                                  }
+                                },
+                                color: Colors.white,
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -120,10 +123,7 @@ class _LoginScreenState extends State<LoginScreen> {
       _emailContoller.clear();
       _passwordController.clear();
 
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
+      Navigator.pushNamed(context, '/dashboard');
     }
   }
 }
