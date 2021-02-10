6e1bf28fca84c8928f67d086450e20e1300cebfa
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-11 00:04:34 +0600

    register succesfull

--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -18,98 +18,101 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.deepPurpleAccent,
-        child: Center(
-          child: Form(
-            key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          color: Colors.deepPurpleAccent,
+          child: Center(
+            child: Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: _nameController,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Name cannot be empty';
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'Name',
-                            labelStyle: TextStyle(
-                              color: Colors.white,
-                            )),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(height: 30),
-                      TextFormField(
-                        controller: _emailContoller,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Email cannot be empty';
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.white)),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(height: 30),
-                      TextFormField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Password cannot be empty';
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.white)),
-                        style: TextStyle(
-                          color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: _nameController,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Name cannot be empty';
+                            } else
+                              return null;
+                          },
+                          decoration: InputDecoration(
+                              labelText: 'Name',
+                              labelStyle: TextStyle(
+                                color: Colors.white,
+                              )),
+                          style: TextStyle(color: Colors.white),
                         ),
-                      ),
-                      SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          FlatButton(
-                            child: Text('Sign Up'),
-                            onPressed: () {
-                              if (_key.currentState.validate()) {
-                                createUser();
-                              }
-                            },
+                        SizedBox(height: 30),
+                        TextFormField(
+                          controller: _emailContoller,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Email cannot be empty';
+                            } else
+                              return null;
+                          },
+                          decoration: InputDecoration(
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: Colors.white)),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        SizedBox(height: 30),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Password cannot be empty';
+                            } else
+                              return null;
+                          },
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: Colors.white)),
+                          style: TextStyle(
                             color: Colors.white,
                           ),
-                          FlatButton(
-                            child: Text('Cancel'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            color: Colors.white,
-                          )
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            FlatButton(
+                              child: Text('Sign Up'),
+                              onPressed: () {
+                                if (_key.currentState.validate()) {
+                                  createUser();
+                                }
+                              },
+                              color: Colors.white,
+                            ),
+                            FlatButton(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
